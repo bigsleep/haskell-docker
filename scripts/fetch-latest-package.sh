5bb7aa6b2182574c85a37203643498118dc3176e
mt <bigsleep.mtkd@gmail.com>
2018-03-06 01:42:22 +0900

    add curl fail option

--- scripts/fetch-latest-package.sh
+++ scripts/fetch-latest-package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 DISTRO=ubuntu
 SERIES=xenial
 ARCH=amd64
 PACKAGE=$1
 
-curl -s "https://api.launchpad.net/1.0/~hvr/+archive/ubuntu/ghc?ws.op=getPublishedBinaries&distro_arch_series=https://api.launchpad.net/1.0/${DISTRO}/${SERIES}/${ARCH}&status=Published&order_by_date=true&binary_name=${PACKAGE}" \
+curl -fs "https://api.launchpad.net/1.0/~hvr/+archive/ubuntu/ghc?ws.op=getPublishedBinaries&distro_arch_series=https://api.launchpad.net/1.0/${DISTRO}/${SERIES}/${ARCH}&status=Published&order_by_date=true&binary_name=${PACKAGE}" \
     | jq "[.entries[] | select(.binary_package_name | test(\"^${PACKAGE}-[0-9]+(\\\\.[0-9]+){1,2}$\",\"x\"))][0]"
