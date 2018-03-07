0ec3d4fbd94e4371201a19f3e99ebd4f859d45df
mt <bigsleep.mtkd@gmail.com>
2018-03-08 01:27:15 +0900

    output binary_package_name

--- scripts/fetch-latest-package.sh
+++ scripts/fetch-latest-package.sh
@@ -8,4 +8,4 @@ ARCH=amd64
 PACKAGE=$1
 
 curl -fs "https://api.launchpad.net/1.0/~hvr/+archive/ubuntu/ghc?ws.op=getPublishedBinaries&distro_arch_series=https://api.launchpad.net/1.0/${DISTRO}/${SERIES}/${ARCH}&status=Published&order_by_date=true&binary_name=${PACKAGE}" \
-    | jq "[.entries[] | select(.binary_package_name | test(\"^${PACKAGE}-[0-9]+(\\\\.[0-9]+){1,2}$\",\"x\"))][0]"
+    | jq -r "[.entries[] | select(.binary_package_name | test(\"^${PACKAGE}-[0-9]+(\\\\.[0-9]+){1,2}$\",\"x\"))][0].binary_package_name"
